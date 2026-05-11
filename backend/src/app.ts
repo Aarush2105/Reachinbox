fa4bb2e5324a793d6b794631982bf300b0176cd2
Aarush Kaushik <Aarushkaushil@gmail.com>
2026-05-11 16:27:28 +0530

    updated url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,7 +5,7 @@ import emailRoutes from "./routes/emailRoutes";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://6a01a936c5d71b85860a76dd--wondrous-horse-7fbe25.netlify.app/",
   credentials: true,
 }));
 app.use(express.json());
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE = axios.create({
-  baseURL: "http://localhost:5000/emails",
+  baseURL: "https://6a01a936c5d71b85860a76dd--wondrous-horse-7fbe25.netlify.app/",
 });
 
 export default BASE;
